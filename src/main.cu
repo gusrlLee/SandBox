beb68ef07cf4c420bb0401c21d4b5ead8446fc6c
gusrlLee <gusrl3204@naver.com>
2026-02-03 00:09:10 +0900

    done modulation

--- src/main.cu
+++ src/main.cu
@@ -1,7 +1,215 @@
 #include <iostream>
+#include <filesystem>
 
-int main()
+#include "math_helper.cuh"
+#include "structs.cuh"
+#include "utils.cuh"
+
+#include <cuda_runtime.h>
+#include <curand_kernel.h>
+
+__global__ void initRandState(int width, int height, curandState *rand_state)
 {
-  std::cout << "Hello world!" << std::endl;
-  return 0;
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if ((i >= width) || (j >= height))
+        return;
+
+    int pIdx = j * width + i;
+    curand_init(1984 + pIdx, 0, 0, &rand_state[pIdx]);
+}
+
+__device__ float3 randomInUnitSphere(curandState *localRandState) {
+    float3 p;
+    do {
+        // -1.0 ~ 1.0 사이의 랜덤 값 추출
+        float r1 = curand_uniform(localRandState);
+        float r2 = curand_uniform(localRandState);
+        float r3 = curand_uniform(localRandState);
+        p = 2.0f * make_float3(r1, r2, r3) - make_float3(1.0f, 1.0f, 1.0f);
+    } while (dot(p, p) >= 1.0f); 
+    return p;
+}
+
+__device__ bool traceRay(
+    const Ray &r, RayPayload &payload,
+    const Triangle *triangles, int numTriangles,
+    const Material *materials, int numMaterials,
+    float tMin, float tMax)
+{
+    RayPayload tmpPayload;
+    bool anyHit = false;
+    float closestSoFar = tMax;
+
+    for (int i = 0; i < numTriangles; i++)
+    {
+        if (triangles[i].intersect(r, tMin, closestSoFar, tmpPayload)) 
+        {
+            anyHit = true;
+            closestSoFar = tmpPayload.t;
+            payload = tmpPayload;
+        }
+    }
+
+    return anyHit;
+}
+
+__device__ float3 radiance
+(
+    Ray &r, int maxDepth,
+    const Triangle *triangles, int numTriangles,
+    const Material *materials, int numMaterials,
+    curandState *randState
+)
+{
+    float3 color = make_float3(0.0f);
+    float3 thp = make_float3(1.0f);
+    Ray ray = r;
+
+    for (int depth = 0; depth < maxDepth; depth++)
+    {
+        RayPayload payload;
+        if (traceRay(ray, payload, triangles, numTriangles, materials, numMaterials, 0.00001f, 1e20f))
+        {
+            const Material &mat = materials[payload.mIdx];
+
+            // L = L + Le
+            color += thp * mat.emission;
+
+            if (mat.type == eDIFFUSE_LIGHT)
+            {
+                break;
+            }
+
+            float3 target = payload.n + randomInUnitSphere(randState);
+            if (dot(target, target) < 1e-8f) 
+            {
+                target = payload.n;
+            }
+
+            Ray outRay(payload.p, normalize(target));
+
+            float3 attenuation = mat.albedo;
+            thp = thp * attenuation;
+            ray = outRay;
+        }
+        else 
+        {
+            break;
+        }
+    }
+    return color;
+}
+
+__global__ void render(
+    float3 *fb,
+    int width, int height, int spp,
+    Camera cam,
+    Triangle *triangles, int numTriangles,
+    Material *materials, int numMaterials,
+    curandState *randState)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if ((i >= width) || (j >= height))
+        return;
+
+    int pIdx = j * width + i;
+    curandState localRandState = randState[pIdx];
+
+    float3 color = make_float3(0.0f);
+
+    for (int s = 0; s < spp; s++)
+    {
+        float u = (float(i) + curand_uniform(&localRandState)) / float(width);
+        float v = (float(j) + curand_uniform(&localRandState)) / float(height);
+
+        Ray ray = Ray(cam.origin, normalize(cam.lowerLeftCorner + u * cam.horizontal + v * cam.vertical - cam.origin));    
+        color += radiance(ray, 5, triangles, numTriangles, materials, numMaterials, &localRandState);
+    }
+
+    color /= float(spp);
+    fb[pIdx] = color;
+    randState[pIdx] = localRandState;
+}
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello world!" << std::endl;
+    std::string inputPath = (argc > 1) ? argv[1] : "./data/CornellBox/CornellBox-Original.obj";
+    std::filesystem::path objPath(inputPath);
+
+    if (!std::filesystem::exists(objPath))
+    {
+        std::cerr << "[Error]: File not found - " << inputPath << std::endl;
+        return -1;
+    }
+
+    std::string objFp = objPath.string();
+    std::string mtlDir = objPath.parent_path().string() + "/";
+
+    std::cout << "[Info] OBJ Path: " << objFp << std::endl;
+    std::cout << "[Info] MTL Dir : " << mtlDir << std::endl;
+
+    int width = 1000;
+    int height = 1000;
+    float aspectRatio = float(width) / float(height);
+    Scene scn = loadScene(objFp, mtlDir);
+
+    Camera cam(
+        make_float3(0.0f, 1.0f, 4.0f), 
+        make_float3(0.0f, 1.0f, 0.0f),
+        make_float3(0.0f, 1.0f, 0.0f), 
+        40.0f, 
+        aspectRatio
+    );
+
+    int numTriangles = scn.triangles.size();
+    int numMaterials = scn.materials.size();
+    size_t triangleMemSize = numTriangles * sizeof(Triangle);
+    size_t materialMemSize = numMaterials * sizeof(Material);
+    size_t fbMemSize = width * height * sizeof(float3);
+    size_t randStateSize = width * height * sizeof(curandState);
+
+    Triangle *dTriangles;
+    Material *dMaterials;
+    float3 *dFrameBuffer;
+    curandState *dRandState;
+
+    cudaMalloc((void **)&dTriangles, triangleMemSize);
+    cudaMalloc((void **)&dMaterials, materialMemSize);
+    cudaMalloc((void **)&dFrameBuffer, fbMemSize);
+    cudaMalloc((void **)&dRandState, randStateSize);
+
+    cudaMemcpy(dTriangles, scn.triangles.data(), triangleMemSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(dMaterials, scn.materials.data(), materialMemSize, cudaMemcpyHostToDevice);
+
+    int tx = 8, ty = 8;
+    dim3 blocks(width / tx + 1, height / ty + 1);
+    dim3 threads(tx, ty);
+
+    initRandState<<<blocks, threads>>>(width, height, dRandState);
+    cudaDeviceSynchronize();
+
+    render<<<blocks, threads>>>(
+        dFrameBuffer, width, height, 20000,
+        cam, dTriangles, numTriangles,
+        dMaterials, numMaterials,
+        dRandState);
+    cudaDeviceSynchronize();
+
+    std::vector<float3> fb(width * height);
+    cudaMemcpy(fb.data(), dFrameBuffer, fbMemSize, cudaMemcpyDeviceToHost);
+
+    std::cout << "[Info] Processing and saving image..." << std::endl;
+    download("output.png", width, height, fb.data());
+
+    cudaFree(dTriangles);
+    cudaFree(dMaterials);
+    cudaFree(dFrameBuffer);
+    cudaFree(dRandState);
+
+    return 0;
 }
---
+++ src/structs.cuh
@@ -0,0 +1,160 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#include <iostream>
+#include <vector>
+
+#include "math_helper.cuh"
+#include "stb/stb_image_write.h"
+
+struct Camera
+{
+    float3 origin;
+    float3 lowerLeftCorner;
+    float3 horizontal;
+    float3 vertical;
+
+    __host__ __device__ Camera() {}
+    __host__ __device__ Camera(float3 lookFrom, float3 lookAt, float3 vup, float vfov, float aspectRatio)
+    {
+        float theta = vfov * (PI / 180.0f);
+        float h = tan(theta / 2.0f);
+
+        float vHight = 2.0f * h;
+        float vWidth = aspectRatio * vHight;
+
+        float3 w = normalize(lookFrom - lookAt);
+        float3 u = normalize(cross(vup, w));
+        float3 v = cross(w, u);
+
+        origin = lookFrom;
+        horizontal = vWidth * u;
+        vertical = vHight * v;
+
+        lowerLeftCorner = origin - horizontal / 2.0f - vertical / 2.0f - w;
+    }
+};
+
+struct Ray
+{
+    float3 orig, dir;
+    __host__ __device__ Ray() {}
+    __host__ __device__ Ray(const float3 &origin, const float3 &direction) : orig(origin), dir(direction) {}
+    __host__ __device__ float3 at(float t) const { return orig + t * dir; }
+};
+
+struct RayPayload
+{
+    float t;       // t-value of intersection
+    float3 p;      // intersected point
+    float3 n;      // normal vector of intersected point
+    uint32_t mIdx; // material index
+};
+
+struct Triangle
+{
+    float3 v0, v1, v2, e1, e2, n;
+    uint32_t matId;
+
+    __host__ __device__ Triangle() {}
+    __host__ __device__ Triangle(const float3 &v0, const float3 &v1, const float3 &v2, const uint32_t &materialId) : v0(v0), v1(v1), v2(v2), matId(materialId)
+    {
+        e1 = v1 - v0;
+        e2 = v2 - v0;
+        n = normalize(cross(e1, e2));
+    }
+    __host__ __device__ float3 normal() const { return n; }
+    __host__ __device__ float3 edge1() const { return e1; }
+    __host__ __device__ float3 edge2() const { return e2; }
+    __host__ __device__ float3 centroid() const { return (v0 + v1 + v2) / 3.0f; }
+    __host__ __device__ float area() const { return length(cross(e1, e2)) / 2.0f; }
+
+    __device__ bool intersect(const Ray &r, float tMin, float tMax, RayPayload &payload) const
+    {
+        float3 v0v1 = v1 - v0;
+        float3 v0v2 = v2 - v0;
+        float3 pvec = cross(r.dir, v0v2);
+
+        float det = dot(v0v1, pvec);
+
+        if (fabs(det) < 1e-8f)
+            return false;
+        float invDet = 1.0f / det;
+
+        float3 tvec = r.orig - v0;
+        float u = dot(tvec, pvec) * invDet;
+        if (u < 0.0f || u > 1.0f)
+            return false;
+
+        float3 qvec = cross(tvec, v0v1);
+        float v = dot(r.dir, qvec) * invDet;
+        if (v < 0.0f || u + v > 1.0f)
+            return false;
+
+        float t = dot(v0v2, qvec) * invDet;
+
+        if (t < tMax && t > tMin)
+        {
+            payload.t = t;
+            payload.p = r.at(t);
+            payload.n = n; // 미리 계산된 법선 사용 (Flat Shading)
+            payload.mIdx = matId;
+            return true;
+        }
+
+        return false;
+    }
+    __device__ bool intersectP(const Ray &r, float tMin, float tMax, RayPayload &payload) const
+    {
+        float3 v0v1 = v1 - v0;
+        float3 v0v2 = v2 - v0;
+        float3 pvec = cross(r.dir, v0v2);
+
+        float det = dot(v0v1, pvec);
+
+        if (fabs(det) < 1e-8f)
+            return false;
+        float invDet = 1.0f / det;
+
+        float3 tvec = r.orig - v0;
+        float u = dot(tvec, pvec) * invDet;
+        if (u < 0.0f || u > 1.0f)
+            return false;
+
+        float3 qvec = cross(tvec, v0v1);
+        float v = dot(r.dir, qvec) * invDet;
+        if (v < 0.0f || u + v > 1.0f)
+            return false;
+
+        float t = dot(v0v2, qvec) * invDet;
+
+        if (t < tMax && t > tMin)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+enum MaterialType
+{
+    eLAMBERTIAN,
+    eDIELECTRIC,
+    eSPECULAR,
+    eDIFFUSE_LIGHT,
+};
+
+struct Material
+{
+    MaterialType type;
+    float3 albedo, emission;
+    __host__ __device__ Material() : albedo(make_float3(0, 0, 0)), emission(make_float3(0, 0, 0)) {}
+};
+
+struct Scene
+{
+    std::vector<Triangle> triangles;
+    std::vector<Material> materials;
+};
+
+#endif
